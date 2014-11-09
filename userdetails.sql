922e525b2929bd58d549a82f5c835c159b7d5726
NVarma <nikita.varma@sjsu.edu>
2014-11-08 21:52:57 -0800

    nodejs, db, download

---
+++ userdetails.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.21-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `userdetails` */
+
+DROP TABLE IF EXISTS `userdetails`;
+
+CREATE TABLE `userdetails` (
+  `firstname` varchar(250) DEFAULT NULL,
+  `lastname` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `userdetails` */
+
+insert  into `userdetails`(`firstname`,`lastname`) values ('\"Nikita\"','\"varma\"'),('\"Deepthi\"','\"Brundavanam\"'),('\"Dipti \"','\"Chavan\"'),('\"Abhishek\"','\"Yadav\"'),('\"Abhishek\"','\"Yadav\"'),('\"Nidhi\"','\"Varma\"'),('\"Amit\"','\"Varma\"');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
